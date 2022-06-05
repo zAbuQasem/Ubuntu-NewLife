a9a974d5308865befbe35179dbd62b03422f99b8
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-06-05 18:43:31 +0300

    Add files via upload

--- Setup.sh
+++ Setup.sh
@@ -27,52 +27,49 @@ sudo apt update -y
 sudo apt install -y build-essential linux-headers-generic dirmngr gnupg apt-transport-https ca-certificates software-properties-common vim git curl wget python-dev python-pip python3-dev python3-pip python3-venv p7zip-full zip unzip netutils gdebi snapd openssh-server vsftpd samba sqlite3 default-jre gdb strace ltrace imagemagick gimp vlc qtwayland5 synaptic audacity telegram-desktop caffeine gnome-shell-extension-weather atril kdeconnect indicator-kdeconnect qtqr obs-studio flameshot chromium-browser wireshark
 
 # Installing ngrok
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee
-              /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb
-              https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee
-              /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt
-              install ngrok -y
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok -y
 # Installing vmware latest version
 function VMWARE() {
-	echo -e "\n[*]Installing VmWare"
+	echo -e "\n[*]Installing VmWare" | tee errors.log
 	wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.vmware.com/go/getplayer-linux
 	chmod +x getplayer-linux
 	sudo ./getplayer-linux --required --eulas-agreed
 	echo "[!] If you faced issues due to kernel update: https://communities.vmware.com/t5/VMware-Workstation-Pro/VMware-16-2-3-not-working-on-Ubuntu-22-04-LTS/td-p/2905535"
-}
+} && VMWARE 2 >> ./errors.log
 
 function SUBLIMETEXT4() {
-	echo -e "\n[*] Installing SublimeText-4 "
+	echo -e "\n[*] Installing SublimeText-4" | tee errors.log
 	curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 	sudo apt update -y && sudo apt -y install sublime-text
-}
+} && SUBLIMETEXT4 2 >> ./errors.log 
 
 function OHMYZSH() {
-	echo -e "\n[*] Installing OhMyZsh"
+	echo -e "\n[*] Installing OhMyZsh" | tee errors.log
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	wget https://raw.githubusercontent.com/zAbuQasem/Misc/main/zshrc
 	sed -i "s/<HOME>/$USER/g" "./zshrc"
 	mv zshrc ~/.zshrc
-}
+} && OHMYZSH 2 >> ./errors.log
 
- function TMUX() {
-	 sudo apt install -y tmux
-	 curl https://raw.githubusercontent.com/zAbuQasem/Misc/main/tmux.conf -o ~/.tmux.conf
-	 # "tpm" plugin manager
-	 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-}
+function TMUX() {
+	echo -e "\n[*] Installing Tmux" | tee errors.log
+	sudo apt install -y tmux
+	curl https://raw.githubusercontent.com/zAbuQasem/Misc/main/tmux.conf -o ~/.tmux.conf
+	# "tpm" plugin manager
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+} && TMUX 2 >> ./errors.log
 
 function VSCODE() {
-	echo -e "\n[*] Installing VScode"
+	echo -e "\n[*] Installing VScode" | tee errors.log
 	sudo apt install -y software-properties-common apt-transport-https wget
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 	sudo apt install -y code
-}
+} && VSCODE 2 >> ./errors.log
 
 function NVIM() {
-	echo -e "\n[*] Installing NeoVim"
+	echo -e "\n[*] Installing NeoVim" | tee errors.log
 	sudo apt-get install neovim
 	wget https://github.com/zAbuQasem/Misc/raw/main/nvim.zip
 	# Just in case lol
@@ -80,53 +77,53 @@ function NVIM() {
 	/usr/bin/unzip nvim.zip –d ~/.config
 	# "Plug" package manager
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-}
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	} && NVIM 2 >> ./errors.log
 
 function FORENSICS() {
-	echo -e "\n[*] Installing Forensics-all package"
+	echo -e "\n[*] Installing Forensics-all package" | tee errors.log
 	sudo apt install -y forensics-all
 }
 
 function OBSIDIAN() {
-	echo -e "\n[*] Installing Obsidian-notes"
+	echo -e "\n[*] Installing Obsidian-notes" | tee errors.log
 	wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/Obsidian-0.14.6.AppImage"
 	sudo mv "./Obsidian-0.14.6.AppImage" /usr/local/bin/obsidian
-}
+} && OBSIDIAN 2 >> ./errors.log
 
 function NOTES() {
-	echo -e "\n[*] Installing MyNotes"
+	echo -e "\n[*] Installing MyNotes" | tee errors.log
 	cd ~/Documents
 	git clone https://github.com/zAbuQasem/MyNotes
 	# Returning to the working directory
 	cd $workingdir
-}
+} && NOTES 2 >> ./errors.log
 
 function DISCORD() {
-	echo -e "\n[*] Installing Discord"
+	echo -e "\n[*] Installing Discord" | tee errors.log
 	wget "https://discord.com/api/download?platform=linux&format=deb"
 	sudo apt install "download?platform=linux&format=deb"
-}
+} && DISCORD 2 >> ./errors.log
 
 function AWSCLI() {
-	echo -e "\n[*] Installing AWS-CLI"
+	echo -e "\n[*] Installing AWS-CLI" | tee errors.log
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
 	sudo ./aws/install
-}
+} && AWSCLI 2 >> ./errors.log
 
 function GHIDRA() {
-	echo -e "\n[*] Installing Ghidra_10.1.3"
+	echo -e "\n[*] Installing Ghidra_10.1.3" | tee errors.log
 	wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.1.3_build/ghidra_10.1.3_PUBLIC_20220421.zip"
 	unzip ghidra_10.1.3_PUBLIC_20220421.zip && mv ghidra_10.1.3_PUBLIC_20220421 ghidra && cp -r ghidra /usr/share
 	wget "https://raw.githubusercontent.com/Crypto-Cat/CTF/main/auto_ghidra.py"
 	chmod +x auto_ghidra.py
 	sed -i '1s/^/#!\/usr\/bin\/env python3 \n/' auto_ghidra.py
 	sudo mv auto_ghidra.py /usr/bin/auto_ghidra
-}
+} && GHIDRA 2 >> ./errors.log
 
 function PWNDBG() {
-	echo -e "\n[*] Installing Pwndbg"
+	echo -e "\n[*] Installing Pwndbg" | tee errors.log
 	git clone https://github.com/pwndbg/pwndbg
 	cd pwndbg
 	./setup.sh
@@ -138,18 +135,29 @@ function PWNDBG() {
 	sudo mv pwndbg /usr/bin
 	# Returning to the working directory
 	cd $workingdir
-}
+} && PWNDBG 2 >> ./errors.log
 
 function PYCHARM() {
 	# Last function to call as it requires GUI interaction
-	echo -e "\n[*] Installing Pycharm"
+	echo -e "\n[*] Installing Pycharm" | tee errors.log
 	curl -fsSL https://raw.githubusercontent.com/nagygergo/jetbrains-toolbox-install/master/jetbrains-toolbox.sh
 	chmod +x jetbrains-toolbox.sh
 	sudo ./jetbrains-toolbox.sh
 }
 
+function I3(){
+	echo -e "\n[*] Installing i3 desktop" | tee errors.log
+	chmod +x "./i3config.sh"
+	./i3config.sh
+} && I3 2 >> ./errors.log
+
 RUN() {
 	# Later going to invoke functions and redirect errors to a file to reinstall manually or fix the script
-	# shutdown -r (Invoke this command at the end to start a new life)
-	echo ""
+	# shutdown -r (Invoke this command at the end to start a new life
+	# Callig pycharm from here, because it requires some GUI interaction, make sure to comment bellow line when runnnig the script remotely!
+	PYCHARM
+	# Comment the below line to skip istalling more than 1000 mb of forensics tools
+	#FORENSICS
+	echo "[+] Finished the setup process, Please view ./errors.log in case of corruption"
+	echo  -e "\033[5mPlease restart your computer!\033[0m"
 }
---
+++ i3config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script is taken from this repo (with slight modifications): https://github.com/xct/kali-clean 
+sudo apt update && sudo apt upgrade -y
+
+sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+
+mkdir -p ~/.local/share/fonts/
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
+
+unzip Iosevka.zip -d ~/.local/share/fonts/
+unzip RobotoMono.zip -d ~/.local/share/fonts/
+
+fc-cache -fv
+
+wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.1-1/alacritty_0.10.1-1_amd64_bullseye.deb
+sudo dpkg -i alacritty_0.10.1-1_amd64_bullseye.deb
+sudo apt install -f
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps && mkdir -p build && cd build && meson ..
+ninja
+sudo ninja install
+cd ../..
+
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/compton
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/alacritty
+cp .config/i3/config ~/.config/i3/config
+cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+cp .config/compton/compton.conf ~/.config/compton/compton.conf
+cp .config/rofi/config ~/.config/rofi/config
+cp .fehbg ~/.fehbg
+cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+cp -r .wallpaper ~/.wallpaper 
+
+echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
+
