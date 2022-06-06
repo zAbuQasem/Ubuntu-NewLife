3cda6646f7b9bffef6813be1a17861c0a7a68cf4
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2022-06-06 16:44:19 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -27,7 +27,7 @@ echo yes | sudo add-apt-repository ppa:obsproject/obs-studio # OBS-Studio screen
 #sudo apt update -y 
 
 # Installing alot of things
-sudo apt install -y build-essential linux-headers-generic dirmngr gnupg apt-transport-https ca-certificates software-properties-common vim git curl wget  python3-dev python3-pip python3-venv p7zip-full zip unzip net-tools gdebi snapd openssh-server vsftpd samba sqlite3 default-jre gdb strace ltrace imagemagick gimp vlc qtwayland5 synaptic audacity telegram-desktop caffeine  atril kdeconnect  qtqr obs-studio flameshot chromium-browser zsh docker.io wireshark golang-go
+sudo apt install -y build-essential linux-headers-generic dirmngr gnupg apt-transport-https ca-certificates software-properties-common vim git curl wget  python3-dev python3-pip python3-venv p7zip-full zip unzip net-tools gdebi snapd openssh-server vsftpd samba sqlite3 default-jre gdb strace ltrace imagemagick gimp vlc qtwayland5 synaptic audacity telegram-desktop caffeine  atril kdeconnect  qtqr obs-studio flameshot chromium-browser zsh docker.io wireshark golang-go plocate
 
 # Adding current user to docker group
 sudo usermod -a -G docker $USER
