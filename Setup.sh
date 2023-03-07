c19c3bdf6a3470d18866d062cb078c1449690cf4
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2023-03-07 22:46:12 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -15,57 +15,52 @@ cd $workingdir
 sudo apt update -y && sudo apt upgrade -y
 
 # Adding repositories
-echo yes | sudo add-apt-repository ppa:gerardpuig/ppa # ubuntu-cleaner
-echo yes | sudo add-apt-repository ppa:videolan/master-daily # VLC
-echo yes | sudo add-apt-repository ppa:eugenesan/ppa # caffeine
-echo yes | sudo add-apt-repository ppa:webupd8team/indicator-kedeconnect # KDE connect
-echo yes | sudo add-apt-repository ppa:obsproject/obs-studio # OBS-Studio screen recorder
 echo yes | sudo add-apt-repository ppa:atareao/atareao # xkb-switch 
 
 
 sudo apt update -y 
 
 # Installing alot of things
-sudo apt install -y nautilus build-essential snapd software-properties-common linux-headers-generic jq dirmngr numlockx brightnessctl xkb-switch exiftool gnupg apt-transport-https gdebi-core ca-certificates software-properties-common vim git curl wget  python3-dev python3-pip python3-distutils python3-venv p7zip-full zip unzip net-tools gdebi snapd openssh-server vsftpd samba sqlite3 default-jre gdb strace ltrace imagemagick gimp vlc qtwayland5 synaptic  telegram-desktop caffeine  atril kdeconnect  qtqr obs-studio flameshot chromium-browser zsh docker.io wireshark golang-go plocate 
+sudo apt install -y nautilus build-essential snapd software-properties-common linux-headers-generic jq dirmngr numlockx brightnessctl xkb-switch exiftool gnupg apt-transport-https gdebi-core ca-certificates software-properties-common vim git curl wget  python3-dev python3-pip python3-distutils python3-venv p7zip-full zip unzip net-tools gdebi  openssh-server vsftpd samba sqlite3 default-jre gdb strace ltrace imagemagick  qtqr obs-studio flameshot chromium-browser zsh docker.io wireshark golang-go plocate
 
 # Adding current user to docker group
 sudo usermod -a -G docker $USER
 
 # Mandatory for i3 headache
-sudo chmod +s /usr/bin/brightnessctl
+#sudo chmod +s /usr/bin/brightnessctl
 
 # Toggle apps by clickig (tested o ubuntu)
 gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize' &>/dev/null
 
 # Installing ngrok , audacity , atom
-echo -e "\n[*]Installing ngrok , audacity , atom " | tee -a errors.log
+echo -e "\n[*]Installing ngrok , audacity , atom " 
 sudo service snapd restart
 sudo systemctl enable snapd
 sudo snap install ngrok
 sudo snap install audacity
-sudo snap install atom --classic
+#sudo snap install atom --classic
 
 
 # Installing vmware latest version
 function VMWARE() {
-	echo -e "\n[*]Installing VmWare" | tee -a errors.log
+	echo -e "\n[*]Installing VmWare"
 	wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.vmware.com/go/getplayer-linux
 	chmod +x getplayer-linux
 	sudo ./getplayer-linux --required --eulas-agreed
 	echo "[!] If you faced issues due to kernel update: https://communities.vmware.com/t5/VMware-Workstation-Pro/VMware-16-2-3-not-working-on-Ubuntu-22-04-LTS/td-p/2905535"
-} && VMWARE
+} #&& VMWARE
 
 function SUBLIMETEXT4() {
-	echo -e "\n[*] Installing SublimeText-4" | tee -a errors.log
+	echo -e "\n[*] Installing SublimeText-4"
 	curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	echo yes | sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 	sudo apt update -y
 	sudo apt install -y sublime-text 
-} && SUBLIMETEXT4  
+} #&& SUBLIMETEXT4  
 
 
 function TMUX() {
-	echo -e "\n[*] Installing Tmux" | tee -a errors.log
+	echo -e "\n[*] Installing Tmux" 
 	sudo apt install -y tmux
 	curl https://raw.githubusercontent.com/zAbuQasem/Misc/main/tmux.conf -o ~/.tmux.conf
 	# "tpm" plugin manager
@@ -73,7 +68,7 @@ function TMUX() {
 } && TMUX 
 
 function VSCODE() {
-	echo -e "\n[*] Installing VScode" | tee -a errors.log
+	echo -e "\n[*] Installing VScode"
 	sudo apt install -y software-properties-common apt-transport-https wget
 	wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 	echo yes | sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
@@ -81,43 +76,41 @@ function VSCODE() {
 } && VSCODE 
 
 function NVIM() {
-	echo -e "\n[*] Installing NeoVim" | tee -a errors.log
+	echo -e "\n[*] Installing NeoVim"
 	# Installing Neovim
 	wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
 	sudo dpkg -i ./nvim-linux64.deb
 	# Installing config file
 	
 	git clone https://github.com/LunarVim/nvim-basic-ide.git ~/.config/nvim
-	# Just in case lol
-	mkdir ~/.config 2>/dev/null
 	# "Plug" package manager
 	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 	} && NVIM 
 
 function FORENSICS() {
-	echo -e "\n[*] Installing Forensics-all package" | tee -a errors.log
+	echo -e "\n[*] Installing Forensics-all package"
 	sudo apt install -y forensics-all
-}
+} #&& FORENSICS
 
 function OBSIDIAN() {
-	echo -e "\n[*] Installing Obsidian-notes" | tee -a errors.log
+	echo -e "\n[*] Installing Obsidian-notes" 
 	wget "https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/obsidian_0.14.6_amd64.deb"
 	chmod +x 'obsidian_0.14.6_amd64.deb'
 	sudo dpkg -i ./'obsidian_0.14.6_amd64.deb'
 
-} && OBSIDIAN 
+} #&& OBSIDIAN 
 
 function NOTES() {
-	echo -e "\n[*] Installing MyNotes" | tee -a errors.log
+	echo -e "\n[*] Installing MyNotes"
 	cd ~/Documents
 	git clone https://github.com/zAbuQasem/MyNotes
 	# Returning to the working directory
 	cd $workingdir
-} && NOTES
+} #&& NOTES
 
 function K8S(){
-	echo -e "\n[*] Installing Minikube" | tee -a errors.log
+	echo -e "\n[*] Installing Minikube"
 	wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 	chmod +x minikube-linux-amd64
 	sudo mv minikube-linux-amd64 /usr/local/bin/minikube
@@ -127,22 +120,22 @@ function K8S(){
 } && K8S
 
 function DISCORD() {
-	echo -e "\n[*] Installing Discord" | tee -a errors.log
+	echo -e "\n[*] Installing Discord"
 	wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
 	sudo dpkg -i $workingdir/discord.deb
-} && DISCORD 
+} #&& DISCORD 
 
 function AWSCLI() {
-	echo -e "\n[*] Installing AWS-CLI" | tee -a errors.log
+	echo -e "\n[*] Installing AWS-CLI"
 	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 	unzip awscliv2.zip
 	sudo ./aws/install
 } && AWSCLI 
 
 function GHIDRA() {
-	echo -e "\n[*] Installing Ghidra_10.1.3" | tee -a errors.log
+	echo -e "\n[*] Installing Ghidra_10.1.3"
 	wget "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.2.2_build/ghidra_10.2.2_PUBLIC_20221115.zip"
-	unzip ghidra_10.2.2_PUBLIC_20221115.zip -d /usr/share/ghidra
+	sudo unzip ghidra_10.2.2_PUBLIC_20221115.zip -d /usr/share/ghidra
 	wget "https://raw.githubusercontent.com/Crypto-Cat/CTF/main/auto_ghidra.py"
 	chmod +x auto_ghidra.py
 	sed -i '1s/^/#!\/usr\/bin\/env python3 \n/' auto_ghidra.py
@@ -158,53 +151,43 @@ function debuggers() {
 
 function PYCHARM() {
 	# Last function to call as it requires GUI interaction
-	echo -e "\n[*] Installing Pycharm" | tee -a errors.log
+	echo -e "\n[*] Installing Pycharm"
 	sudo snap install pycharm-community --classic
-} && PYCHARM
+} #&& PYCHARM
 
 
 function PYTHONLIBS(){
-	echo -e "\n[*] Installing Python3 packages" | tee -a errors.log
+	echo -e "\n[*] Installing Python3 packages"
 	pip3 install --no-warn-script-location  updog trufflehog rich flask-unsign flask paramiko pyngrok pwntools z3-solver gitdumper pwncat-cs
-} && PYTHONLIBS
+} #&& PYTHONLIBS
 
 function MSTEAMS() {
-	echo -e "\n[*] Installing Microsoft teams" | tee -a errors.log
+	echo -e "\n[*] Installing Microsoft teams"
 	# Ensuring sudo pass again...
 	sudo ls
 	curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 	sudo apt update -y && sudo apt install -y teams
-} && MSTEAMS
+} #&& MSTEAMS
 
 function GITHUBDESKTOP(){
-	echo -e "\n[*] Installing Github desktop" | tee -a errors.log
+	echo -e "\n[*] Installing Github desktop"
 	wget https://github.com/shiftkey/desktop/releases/download/release-2.9.3-linux3/GitHubDesktop-linux-2.9.3-linux3.deb
 	echo y | sudo gdebi GitHubDesktop-linux-2.9.3-linux3.deb
-} && GITHUBDESKTOP
-
-function sliverc2(){
-	echo -e "\n[*] Installing Github desktop" | tee -a errors.log
-	curl https://sliver.sh/install|sudo bash
-} && sliverc2
+} #&& GITHUBDESKTOP
 
 function I3(){
-	echo -e "\n[*] Installing i3 desktop" | tee -a errors.log
+	echo -e "\n[*] Installing i3 desktop"
 	cd $basedir
 	chmod +x "./i3config.sh"
 	./i3config.sh
 } && I3 
 
+
 RUN() {
-	# Later going to invoke functions and redirect errors to a file to reinstall manually or fix the script
-	# shutdown -r (Invoke this command at the end to start a new life
-	# Comment the below line to skip istalling more than 1000 mb of forensics tools
-	#FORENSICS
 	# Cleanups
 	sudo apt-get clean
 	sudo apt -y autoremove
 	echo "[+] Finished the setup process, Please view ./errors.log in case of corruption"
 	echo  -e "\033[5m---> Please restart your computer!\033[0m"
 } && RUN
-# TODO
-# Install the apps that require appending a source file from snap if they are available
