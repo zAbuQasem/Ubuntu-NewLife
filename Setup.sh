fba5e80ae0a2de91d16c706a9313f20af5f6510c
zAbuQasem <74984708+zAbuQasem@users.noreply.github.com>
2023-03-11 07:54:54 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -52,7 +52,7 @@ sudo snap install audacity
 
 # Install golang
 wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+sudp rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # Installing vmware latest version
