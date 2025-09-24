52fb72eb4e70543143f74b4521fb19a0bb8a104d
zAbuQasem <zeyad.abuqasem@gmail.com>
2025-09-25 01:22:59 +0300

    Update i3lock-color script: add theme support with customizable colors

--- .config/i3/scripts/i3lock-color.sh
+++ .config/i3/scripts/i3lock-color.sh
@@ -1,13 +1,98 @@
 #!/bin/sh
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#ff00ffcc'
-TEXT='#ee00eeee'
-WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+# Pick a theme by exporting I3LOCK_THEME before running this script.
+# Supported: catppuccin, nord, gruvbox, tokyo, dracula
+# If not set, pick one at random each run (POSIX sh-compatible randomness).
+if [ -n "$I3LOCK_THEME" ]; then
+	THEME="$I3LOCK_THEME"
+else
+	THEMES="catppuccin nord gruvbox tokyo dracula"
+	# Prefer /dev/urandom via od for randomness; fallback to awk srand()
+	if command -v od >/dev/null 2>&1; then
+		idx=$(od -An -N2 -tu2 /dev/urandom 2>/dev/null | tr -d ' ')
+		idx=$(( (idx % 5) + 1 ))
+	else
+		idx=$(awk 'BEGIN{srand(); print int(1+rand()*5)}')
+	fi
+	i=1
+	for t in $THEMES; do
+		if [ "$i" -eq "$idx" ]; then
+			THEME="$t"
+			break
+		fi
+		i=$((i+1))
+	done
+fi
+
+case "$THEME" in
+	catppuccin)
+		# Catppuccin Mocha
+		BLANK='#00000000'
+		CLEAR='#1e1e2e99'      # overlay (60% alpha)
+		DEFAULT='#89b4faff'    # accent (blue)
+		TEXT='#cdd6f4ff'       # text (latte)
+		WRONG='#f38ba8ff'      # red
+		VERIFYING='#a6e3a1ff'  # green
+		KEYHL='#fab387ff'      # peach
+		BSHL='#f38ba8ff'       # red
+		;;
+	nord)
+		BLANK='#00000000'
+		CLEAR='#2e344099'
+		DEFAULT='#88c0d0ff'
+		TEXT='#e5e9f0ff'
+		WRONG='#bf616aff'
+		VERIFYING='#a3be8cff'
+		KEYHL='#d08770ff'
+		BSHL='#bf616aff'
+		;;
+	gruvbox)
+		BLANK='#00000000'
+		CLEAR='#28282899'
+		DEFAULT='#83a598ff'
+		TEXT='#fbf1c7ff'
+		WRONG='#fb4934ff'
+		VERIFYING='#b8bb26ff'
+		KEYHL='#d79921ff'
+		BSHL='#fb4934ff'
+		;;
+	tokyo)
+		BLANK='#00000000'
+		CLEAR='#1a1b2699'
+		DEFAULT='#7aa2f7ff'
+		TEXT='#c0caf5ff'
+		WRONG='#f7768eff'
+		VERIFYING='#9ece6aff'
+		KEYHL='#bb9af7ff'
+		BSHL='#f7768eff'
+		;;
+	dracula)
+		BLANK='#00000000'
+		CLEAR='#282a3699'
+		DEFAULT='#bd93f9ff'
+		TEXT='#f8f8f2ff'
+		WRONG='#ff5555ff'
+		VERIFYING='#50fa7bff'
+		KEYHL='#ffb86cff'
+		BSHL='#ff5555ff'
+		;;
+	*)
+		# Sensible fallback
+		BLANK='#00000000'
+		CLEAR='#00000088'
+		DEFAULT='#5294e2ff'
+		TEXT='#f0f0f0ff'
+		WRONG='#ff6b6bff'
+		VERIFYING='#8bd5aaff'
+		KEYHL='#ffae56ff'
+		BSHL='#ff6b6bff'
+		;;
+esac
 
 i3lock \
+--blur 12 \
+--radius 120 \
+--ring-width 9 \
 --insidever-color=$CLEAR     \
 --ringver-color=$VERIFYING   \
 \
@@ -24,11 +109,10 @@ i3lock \
 --time-color=$TEXT           \
 --date-color=$TEXT           \
 --layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
+--keyhl-color=$KEYHL         \
+--bshl-color=$BSHL           \
 \
 --screen 2                   \
---blur 5                     \
 --clock                      \
 --indicator                  \
 --time-str="%H:%M:%S"        \
